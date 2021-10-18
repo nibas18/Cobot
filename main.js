edba81ae246af9169b9af541cf8ea92ea25171ef
NaimoIbrahim75 <43139535+NaimoIbrahim75@users.noreply.github.com>
2021-10-18 18:26:53 +0200

    Map html css
    Made a visual presentation of the map page using html and css, haven't done some backend script coding.  I have also able to other pages e.g. starting page, and login. I am also creating a signup html file, still missing designing it via css.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow } = require('electron')
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1366,
+        height: 1024
     })
-
+    const electron = require('electron')
+    require('electron-reload')(__dirname);
     win.loadFile('index.html')
 }
 app.whenReady().then(() => {
