4813478e19957a700e87e20fb23afbcf606a1186
nibas18 <43138810+nibas18@users.noreply.github.com>
2021-10-27 15:22:18 +0200

    robots are moving

--- entity.js
+++ entity.js
@@ -5,7 +5,7 @@ class Entity {
         this.position = position;
         this.scale = 1;
         this.behaviours.forEach(element => {
-            element.gameObject = this;
+            element.entity = this;
         });
     }
 
@@ -19,12 +19,12 @@ class Entity {
         this.position.updateScale();
 
         this.behaviours.forEach(element => {
-            if (element.enabled) {
-                element.update();
-                if (debugMode) {
-                    element.onDebug();
-                }
+            // if (element.enabled) {
+            element.update();
+            if (debugMode) {
+                element.onDebug();
             }
+            // }
 
         });
     }
@@ -41,7 +41,7 @@ class Entity {
 
 /* ------Behaviours------ */
 class Behaviour {
-    enabled = true;
+    /*enabled = true;
 
     setEnabled(value) {
         this.enabled = value;
@@ -50,7 +50,7 @@ class Behaviour {
         } else {
             this.onDisable()
         }
-    }
+    }*/
 
     awake() { }
     update() { }
@@ -81,13 +81,35 @@ class SpriteRenderer extends Behaviour {
 
     updateProperties() {
         this.spriteScale = this.image.width / this.image.height;
-        this.width = this.scale * this.gameObject.scale * scale * this.spriteScale;
-        this.height = this.scale * this.gameObject.scale * scale;
-        this.position.x = this.gameObject.position.x - this.width * 0.5;
-        this.position.y = this.gameObject.position.y - this.height * 0.5;
+        this.width = this.scale * this.entity.scale * scale * this.spriteScale;
+        this.height = this.scale * this.entity.scale * scale;
+        this.position.x = this.entity.position.x - this.width * 0.5;
+        this.position.y = this.entity.position.y - this.height * 0.5;
     }
 
     draw() {
         ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+class Movement extends Behaviour {
+    constructor(speed) {
+        super();
+        this.speed = speed;
+        this.target = null;
+    }
+
+    update() {
+        if (this.target != null) {
+            let velocity = this.entity.position.directionTo(this.target.position);
+            velocity.normalize();
+
+            let move = multiplyVectorByScalar(velocity, this.speed);
+
+            let currentPos = this.entity.position;
+
+            this.entity.position.setScale(move.x + currentPos.x,  move.y + currentPos.y);
+
+        }
     }
 }
