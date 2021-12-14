e03e95d3435a740a0309c43eba332536290e089d
nibas18 <43138810+nibas18@users.noreply.github.com>
2021-12-14 10:02:21 +0100

    pictures of tables have been added

--- html/zones.js
+++ html/zones.js
@@ -10,7 +10,7 @@ class Zone {
         var c = document.getElementById("canvas")
         var ctx = c.getContext("2d")
         // image 
-        // var img = document.getElementById("table");
+        var img = document.getElementById("table");
         ctx.beginPath();
         //zonen
         ctx.rect(start_x, start_y, this.width,this.height);
@@ -19,17 +19,16 @@ class Zone {
         // looper igennem de forskellige borde. 
         for (const [key, value] of Object.entries(this.tables)){
             ctx.rect(value[0]+this.start_x, value[1]+this.start_y, value[2], value[3]);
-            ctx.stroke();
-        //      img.onload = function (){
-        //         ctx.drawImage(img, value[0]+this.start_x, value[1]+this.start_y, 15, 15);
-        //      }
-        // }
-
+            //ctx.stroke();
+            ctx.drawImage(img, value[0]+this.start_x, value[1]+this.start_y, 30, 30);
+              //img.onload = function (){
+                 //}
+         }
     }
-    }
-
-
 }
+
+
+
 
 // start_x og start_y, width, height kan stores i jeres database under tablet "zones"
 //var zone1 = new Zone(20, 20, 160,160, {table1: [40, 40, 10,10],table2:[60,60, 10, 10],table3:[80,80,10,10]})
