9aa115a92ee1bbc6b086e66495cd1fdc2b9cf5da
nibas18 <43138810+nibas18@users.noreply.github.com>
2021-12-20 10:26:08 +0100

    The tables in the middle have been fixed

--- html/cafeteria_map.php
+++ html/cafeteria_map.php
@@ -29,8 +29,8 @@
 
             <script>
                 let zone1 = new Zone(9, 133, 221, 91, { table1: [20, 30, 30, 28], table2: [100, 30, 30, 28], table3: [170, 30, 30, 28] })
-                let zone2 = new Zone(9, 224, 191, 74, { table1: [20, 25, 20, 40], table2: [100, 25, 10, 10], table3: [155, 25, 40, 28] })
-                let zone3 = new Zone(9, 298, 191, 81, { table1: [20, 25, 10, 10], table2: [100, 25, 30, 10], table3: [155, 25, 40, 28] })
+                let zone2 = new Zone(9, 224, 191, 74, { table1: [20, 25, 20, 40], table2: [85, 25, 10, 10], table3: [155, 25, 40, 28] })
+                let zone3 = new Zone(9, 298, 191, 81, { table1: [20, 25, 10, 10], table2: [85, 25, 10, 10], table3: [155, 25, 40, 28] })
                 let zone4 = new Zone(200, 224, 106, 155, { table1: [20, 25, 10, 10], table2: [20, 100, 10, 10], table3: [60, 60, 10, 10] })
 
                 let t = new Zone();
