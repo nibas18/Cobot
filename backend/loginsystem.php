3c9de75ad4ec1dc344827b8a5a85ed8e193621eb
NaimoIbrahim75 <43139535+NaimoIbrahim75@users.noreply.github.com>
2022-01-03 12:43:47 +0100

    select caf update - all three images clickable function work, removed couple commeted code.

--- backend/loginsystem.php
+++ backend/loginsystem.php
@@ -4,17 +4,13 @@
     //Login System:    
     if(isset($_POST['login'])) {
         $mobile = $_POST['mobile'];
-        
         $sql = "SELECT * FROM Users WHERE Mobile ='$mobile'";
         $query_result = mysqli_query($db_connection, $sql);
-        //$row = mysqli_fetch_assoc($query_result);
         $row = mysqli_fetch_array($query_result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($query_result);
         if($count > 0){
-            //header("Location: ../html/main_use_page.php");
             $Personalid = $row['Personalid'];
             $_SESSION['Personalid'] = $Personalid;
-            //$row["Personalid"] = $_SESSION["Personalid"];
             header("location: ../html/main_use_page.php");
         }else{
             $error = "Error the number is Invalid";
--- backend/selection.js
+++ backend/selection.js
@@ -1,6 +1,17 @@
 function tekcafFunction() {
-    console.log("he");
-    //document.getElementsById("tek").style.borderColor = "#2DE1CC";
+    console.log("The tek cafeteria has been selected.");
     document.getElementById("tek").style.borderColor = "#2DE1CC";
+    window.location.href = "../html/cafeteria_map.php";
+}
+
+function bigcafFunction() {
+    console.log("The big cafeteria has been selected.");
+    document.getElementById("bg").style.borderColor = "#2DE1CC";
+    window.location.href = "../html/cafeteria_map.php";
+}
+
+function smallcafFunction() {
+    console.log("The small cafeteria has been selected.");
+    document.getElementById("sm").style.borderColor = "#2DE1CC";
     window.location.href = "../html/cafeteria_map.php";
 }
